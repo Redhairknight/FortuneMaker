f60fdc66b3337bd93b12ea6483ce451314e21932
XinZheng-SH <xin.zheng1024@gmail.com>
2020-09-17 11:19:52 +1000

    Update donationCharityBrief.js

--- screens/donationCharityBrief.js
+++ screens/donationCharityBrief.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ Component } from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import { StyleSheet, Text, View ,Image, ScrollView} from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -9,25 +9,40 @@ function donationCharityBrief({navigation}){
   return (
     <View style={styles.container}>
 
-      <View style={{flex:1, flexDirection:"column"}}>
-     <View style={{flex:1}}>
-        <View style={{ width: 380,  height: 150,backgroundColor: '#1F4E79'}}></View>
-      <Text style={{color:'white', fontSize:50,textAlign:"center",top:'-35%'}}>Charity detail</Text>
+    <View
+        style={{
+          backgroundColor: "#1F4E79",
+          width: "100%",
+          height:"15%"
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 40,
+            color:"white",
+            marginTop:"7%"
+          }}
+        >
+          Charity Info
+        </Text>
       </View>
-      </View>
-      
-      <View style={{flex:1, flexDirection:"row"}}>
+
+
+      {/**主要内容 */}
+      <View style={{flexDirection:"row"}}>
         <View style={{flex:1}}>
-       <Image style={{right:'-5%',top:'-5%'}} source={require('../assets/check1.png')} />
+       <Image style={{right:'-5%',}} source={require('../assets/check1.png')} />
        <Text style={{textAlign:"center",top:'5%'}} >Australian Koala Foundation </Text>
        </View>
        <View style={{flex:1}}>
-       <Image style={{left:'5%',top:'-5%'}} source={require('../assets/check2.png')} />
+       <Image style={{left:'5%',}} source={require('../assets/check2.png')} />
        <Text style={{textAlign:"center",top:'5%'}}>Guide Dog</Text>
        </View>
        </View>
 
-       <View style={{flex:1, flexDirection:"row"}}>
+      <View style={{flexDirection:"row"}}>
+      
        <View style={{flex:1}}>
        <Image style={{right:'-5%'}} source={require('../assets/check3.png')} />
        <Text onPress={()=>navigation.navigate('DonationCharityCanTeen')} style={{textAlign:"center",top:'5%'}}>CanTeen</Text>
@@ -37,14 +52,12 @@ function donationCharityBrief({navigation}){
        <Text style={{textAlign:"center",top:'5%'}}> Royal flying doctor service </Text>
        </View>
        </View>
+      
+
        
-       <View style={{width: 380, height: 200, backgroundColor: '#1F4E79'}} >
-       <Text style={{color:'white',textAlign:"center"}} >Note: Fortune maker provides fund-raising services on the Internet platform, and every donation made by users goes directly to the account of the charity.</Text>
-       </View>
 
        
       
-      <StatusBar style="auto" />
     </View>  
   );
   } 
